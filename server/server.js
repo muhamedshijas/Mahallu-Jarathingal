278dbca19bbfd043588c0eae3d44cc089fec56e1
muhamedshijas <shijushijas157@gmail.com>
2023-09-07 09:58:40 +0530

    database connected

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 import express from 'express'
+import dbConnect from './Config/dbConnect.js'
 const app= new express()
 
-
-
-
-
+dbConnect()
 app.listen(5000,()=>{
     console.log("app running on port 5000")
 })
